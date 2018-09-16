b023e9f16e46b93321e7e359e7b763706ccffe06
Yuichi Fujiki <yuichi@chaione.com>
2018-09-16 12:41:24 -0500

    Writing first Android Test.

---
+++ app/src/androidTest/java/com/yfujiki/gradeconverter/GradeSystemTableTest.kt
@@ -0,0 +1,23 @@
+package com.yfujiki.gradeconverter
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.yfujiki.gradeconverter.Models.GradeSystemTable
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class GradeSystemTableTest {
+
+    @Test
+    fun testInit() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        GradeSystemTable.init(appContext)
+
+        assertEquals(17, GradeSystemTable.tableBody.count())
+        assertTrue(GradeSystemTable.tableBody.keys.contains("Yosemite Decimal System-Sports"))
+        assertTrue(GradeSystemTable.tableBody.keys.contains("Brazil-Boulder"))
+        assertEquals(30, GradeSystemTable.tableBody["Yosemite Decimal System-Sports"]?.grades?.count())
+    }
+}
